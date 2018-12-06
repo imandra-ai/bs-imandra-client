7597c364b63cd9aeb82e6dec6e33d52c4ca45886
Dave Aitken <dave.aitken@gmail.com>
2018-12-06 15:10:51 +0000

    tidy: tests with new startup failure behavior

--- src/__tests__/Imandra_client_tests.ml
+++ src/__tests__/Imandra_client_tests.ml
@@ -10,67 +10,59 @@ let () =
           runningImandraProcess := Some ip;
           Js.Promise.resolve ()
         )
+      |> Js.Promise.catch (fun e ->
+          Js.Console.error e;
+          Js.Promise.reject (Failure "Problem spawning Imandra process");
+        )
     )
 
 let () =
   testPromise "verify refuted" (fun () ->
-      match !runningImandraProcess with
-      | Some ip ->
-        Imandra_client.Verify.by_src ip ~src:"fun x -> x = 3"
-        |> Js.Promise.then_ (function
-            | Belt.Result.Ok (Imandra_client.Verify.Refuted _, _) -> Js.Promise.resolve pass
-            | Belt.Result.Ok _ -> Js.Promise.resolve (fail "wrong verify result")
-            | Belt.Result.Error (e, _) -> Js.Promise.resolve (fail (Printf.sprintf "error from imandra: %s" e))
-          )
-      | None ->
-        Js.Promise.reject (Failure "no imandra process available?")
+      let ip = !runningImandraProcess |> Belt.Option.getExn in
+      Imandra_client.Verify.by_src ip ~src:"fun x -> x = 3"
+      |> Js.Promise.then_ (function
+          | Belt.Result.Ok (Imandra_client.Verify.Refuted _, _) -> Js.Promise.resolve pass
+          | Belt.Result.Ok _ -> Js.Promise.resolve (fail "wrong verify result")
+          | Belt.Result.Error (e, _) -> Js.Promise.resolve (fail (Printf.sprintf "error from imandra: %s" e))
+        )
     )
 
 let () =
   testPromise "verify by name proved" (fun () ->
-      match !runningImandraProcess with
-      | Some ip ->
-        Imandra_client.Eval.by_src ip ~src:"let rev_rev x = 3 = 3"
-        |> Js.Promise.then_ (function
-            | Belt.Result.Ok _ ->
-              Imandra_client.Verify.by_name ip ~name:"rev_rev"
-              |> Js.Promise.then_ (function
-                  | Belt.Result.Ok (Imandra_client.Verify.Proved, _) -> Js.Promise.resolve pass
-                  | Belt.Result.Ok _ -> Js.Promise.resolve (fail "wrong verify result")
-                  | Belt.Result.Error (e, _) -> Js.Promise.resolve (fail (Printf.sprintf "error from imandra: %s" e))
+      let ip = !runningImandraProcess |> Belt.Option.getExn in
+      Imandra_client.Eval.by_src ip ~src:"let rev_rev x = 3 = 3"
+      |> Js.Promise.then_ (function
+          | Belt.Result.Ok _ ->
+            Imandra_client.Verify.by_name ip ~name:"rev_rev"
+            |> Js.Promise.then_ (function
+                | Belt.Result.Ok (Imandra_client.Verify.Proved, _) -> Js.Promise.resolve pass
+                | Belt.Result.Ok _ -> Js.Promise.resolve (fail "wrong verify result")
+                | Belt.Result.Error (e, _) -> Js.Promise.resolve (fail (Printf.sprintf "error from imandra: %s" e))
               )
-            | Belt.Result.Error (e, _) -> Js.Promise.resolve (fail (Printf.sprintf "error from imandra: %s" e))
-          )
-      | None ->
-        Js.Promise.reject (Failure "no imandra process available?")
+          | Belt.Result.Error (e, _) -> Js.Promise.resolve (fail (Printf.sprintf "error from imandra: %s" e))
+        )
     )
 
 let () =
   testPromise "instance" (fun () ->
-      match !runningImandraProcess with
-      | Some ip ->
-        Imandra_client.Instance.by_src ip ~src:"fun x -> List.length x > 4"
-        |> Js.Promise.then_ (function
-            | Belt.Result.Ok (Imandra_client.Instance.Sat _, _) -> Js.Promise.resolve pass
-            | Belt.Result.Ok (_, _) -> Js.Promise.resolve (fail "instance result not satisifed")
-            | Belt.Result.Error (e, _) -> Js.Promise.resolve (fail (Printf.sprintf "error from imandra: %s" e))
-          )
-      | None ->
-        Js.Promise.reject (Failure "no imandra process available?")
+      let ip = !runningImandraProcess |> Belt.Option.getExn in
+      Imandra_client.Instance.by_src ip ~src:"fun x -> List.length x > 4"
+      |> Js.Promise.then_ (function
+          | Belt.Result.Ok (Imandra_client.Instance.Sat _, _) -> Js.Promise.resolve pass
+          | Belt.Result.Ok (_, _) -> Js.Promise.resolve (fail "instance result not satisifed")
+          | Belt.Result.Error (e, _) -> Js.Promise.resolve (fail (Printf.sprintf "error from imandra: %s" e))
+        )
     )
 
 let () =
   testPromise "eval failure" (fun () ->
-      match !runningImandraProcess with
-      | Some ip ->
-        Imandra_client.Instance.by_src ip ~src:"garbage"
-        |> Js.Promise.then_ (function
-            | Belt.Result.Ok (_, _) -> Js.Promise.resolve (fail "unexpected success")
-            | Belt.Result.Error (e, _) ->
-              Js.Promise.resolve (Expect.toEqual e (`Just "Unbound value garbage"))
-          )
-      | None ->
-        Js.Promise.reject (Failure "no imandra process available?")
+      let ip = !runningImandraProcess |> Belt.Option.getExn in
+      Imandra_client.Instance.by_src ip ~src:"garbage"
+      |> Js.Promise.then_ (function
+          | Belt.Result.Ok (_, _) -> Js.Promise.resolve (fail "unexpected success")
+          | Belt.Result.Error (e, _) ->
+            Js.Promise.resolve (Expect.toEqual e (`Just "Unbound value garbage"))
+        )
     )
 
 let () =
